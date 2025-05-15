af0473063f4cc40f8c5b0d7115294848f1272328
Danh0504X <Danhvt.504.dev@gmail.com>
2025-05-15 22:57:38 +0700

    Update CSD_S2

--- CSD201/CSD201_S2_LinkedList/src/main/java/App/main.java
+++ CSD201/CSD201_S2_LinkedList/src/main/java/App/main.java
@@ -1,9 +1,10 @@
+package App;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
-package view;
 
 import controller.studentController;
 import model.LinkedListStudentModel;
