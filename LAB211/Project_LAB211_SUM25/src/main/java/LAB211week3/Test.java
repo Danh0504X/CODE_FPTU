5292a6179083fd7aec10a0bd9cc43910c3eef916
Danh0504X <Danhvt.504.dev@gmail.com>
2025-06-06 03:56:22 +0700

    Lab week 3

---
+++ LAB211/Project_LAB211_SUM25/src/main/java/LAB211week3/Test.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB211week2;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Test {
+   public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("======== LAB WEEK2 PROGRAM MENU ========");
+            System.out.println("1. Short 51 - Calculator + BMI");
+            System.out.println("2. Short 61 - Shape Calculator");
+            System.out.println("3. Short 74  - Matrix");
+            System.out.println("4. Exit");
+            System.out.print("Please choose one option: ");
+            choice = Validator.getChoice(sc, 1, 4);
+
+            switch (choice) {
+                case 1:
+                    MenuMayTinh calculatorApp = new MenuMayTinh(sc);
+                    calculatorApp.start();  
+                    break;
+                case 2:
+                    ShapeProgram shapeApp = new ShapeProgram();
+                    shapeApp.start();  
+                case 3:
+                    new MatrixProgram(sc).start(); 
+                    break;
+                case 4:
+                    System.out.println("Exiting main program...");
+                    break;
+            }
+        } while (choice != 4);
+
+        sc.close();
+    }
+}
